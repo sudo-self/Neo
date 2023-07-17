dc6b6251581fd47ac9b222784c7fd2aad286b1b5
Sudo-Self <sudo-self@proton.me>
2023-07-16 22:33:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let animation;
+
+const delay = time => new Promise(r => setTimeout(r, time));
+
+const animate = async () => {
+    animation = true;
+  
+    term.clear().set_prompt('');
+    await term.set_prompt('Wake up...', { typing: true });
+    await delay(5000);
+    term.set_prompt('');
+    await term.set_prompt('The Matrix has you...', { typing: true });
+    await delay(5000);
+    term.set_prompt('');
+    await term.set_prompt('Follow the white rabbit.', { typing: true });
+    await delay(5000);
+    term.set_prompt('Knock, knock, Karla.');
+    await delay(5000);
+    term.set_prompt('');
+    animation = false;
+};
+
+const unasync = function(fn) {
+    return () => {
+        fn();
+    };
+};
+
+const term = $('body').terminal(unasync(animate), {
+    prompt: '',
+    greetings: false,
+    keydown: () => animation ? false : undefined
+});
+
+animate();
+
